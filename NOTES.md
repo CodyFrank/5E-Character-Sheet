5e3b40ae06deca7a1b0b3b026b22813ad989b43d
cody_frank <cody.frank30@gmail.com>
2020-02-22 16:43:08 -0500

    creates character table

---
+++ NOTES.md
@@ -0,0 +1,6 @@
+
+[] create character table
+[] TEST MODELS!!!
+[] finish characters controller actions
+[] add serializers
+[] add has many / belongs too relationship
--- character-sheet-api/db/migrate/20200222011659_create_characters.rb
+++ character-sheet-api/db/migrate/20200222011659_create_characters.rb
@@ -2,6 +2,87 @@ class CreateCharacters < ActiveRecord::Migration[6.0]
   def change
     create_table :characters do |t|
 
+      t.string :name
+      t.integer :level
+      t.string :class
+      t.string :back_ground
+      t.string :player_name
+      t.string :race
+      t.string :alignment
+      t.integer :experience
+      t.string :inspiration
+      t.integer :proficiency_bonus
+      t.integer :strength
+      t.integer :dexterity
+      t.integer :constitution
+      t.integer :intelligence
+      t.integer :wisdom
+      t.integer :charisma
+      t.integer :strength_modifier
+      t.integer :dexterity_modifier
+      t.integer :constitution_modifier
+      t.integer :intelligence_modifier
+      t.integer :wisdom_modifier
+      t.integer :charisma_modifier
+      t.integer :strength_saving_throw
+      t.integer :dexterity_saving_throw
+      t.integer :constitution_saving_throw
+      t.integer :intelligence_saving_throw
+      t.integer :wisdom_saving_throw
+      t.integer :charisma_saving_throw
+      t.integer :acrobatics_modifier
+      t.integer :animal_handling_modifier
+      t.integer :arcana_modifier
+      t.integer :athletics_modifier
+      t.integer :deception_modifier
+      t.integer :history_modifier
+      t.integer :insight_modifier
+      t.integer :intimidation_modifier
+      t.integer :investigation_modifier
+      t.integer :medicine_modifier
+      t.integer :nature_modifier
+      t.integer :perception_modifier
+      t.integer :performance_modifier
+      t.integer :persuasion_modifier
+      t.integer :religion_modifier
+      t.integer :slight_of_hand_modifier
+      t.integer :stealth_modifier
+      t.integer :survival_modifier
+      t.integer :passive_perception
+      t.text :other_proficiencies_and_languages
+      t.integer :armor_class
+      t.integer :initiative
+      t.integer :speed
+      t.integer :hit_point_maximum
+      t.integer :current_hit_points
+      t.integer :temporary_hit_points
+      t.integer :hit_dice
+      t.integer :sucessful_death_saves
+      t.integer :failed_death_saves
+      t.integer :cp
+      t.integer :sp
+      t.integer :ep
+      t.integer :gp
+      t.integer :pp
+      t.text :personality_traits
+      t.text :ideals
+      t.text :bonds
+      t.text :flaws
+      t.text :features_and_traits
+      t.integer :age
+      t.integer :height
+      t.integer :weight
+      t.string :eyes
+      t.string :skin
+      t.string :hair
+      t.text :allies_and_organizations
+      t.text :character_backstory
+      t.text :treasure
+      t.string :spellcasting_ability
+      t.integer :spell_save_dc
+      t.integer :spell_attack_bonus
+
+
       t.timestamps
     end
   end
