e61063d67e73a518b8f5e7a8c1e26d7f4e160c0a
cody_frank <cody.frank30@gmail.com>
2020-02-23 14:51:40 -0500

    imports and creates store with redux dev tools extension

--- character-sheet-frontend/src/index.js
+++ character-sheet-frontend/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
