c7c201ddf28378b12d116a796ee2cdc08c4d7a15
cody_frank <cody.frank30@gmail.com>
2020-02-23 16:24:39 -0500

    sets up / and /characters routes in app.js

--- character-sheet-frontend/src/index.js
+++ character-sheet-frontend/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import rootReducer from './reducers'
 
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
