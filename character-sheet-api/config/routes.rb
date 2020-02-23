9fb4562f1ebe72ca9ff825d8f4ce7af996973704
cody_frank <cody.frank30@gmail.com>
2020-02-22 19:07:59 -0500

    adds namespace api v1 to contorller and routes

--- character-sheet-api/config/routes.rb
+++ character-sheet-api/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :characters
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :characters
+    end
+  end
 end
