33837771533b6b94b59335ee8701b26f1f6680eb
cody_frank <cody.frank30@gmail.com>
2020-02-22 21:26:54 -0500

    creates characters index and show actions

--- character-sheet-api/app/controllers/api/v1/characters_controller.rb
+++ character-sheet-api/app/controllers/api/v1/characters_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::CharactersController < ApplicationController
+    def index
+        characters = Character.all
+        render json: CharacterSerializer.new(characters).to_serialized_json
+    end
+
+    def show
+        if character = Character.find_by(id: params[:id])
+            render json: CharacterSerializer.new(character).to_serialized_json
+        else
+            render json: {message: "Cannot find a Character with that ID"}
+        end
+    end
 end
