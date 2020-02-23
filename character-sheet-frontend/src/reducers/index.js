ab7f093c68f26c37fe6f4e0acab1d4096aab5284
cody_frank <cody.frank30@gmail.com>
2020-02-23 15:51:07 -0500

    creates rootReducer using combine reducer and other reducer functions

---
+++ character-sheet-frontend/src/reducers/index.js
@@ -0,0 +1,14 @@
+import { attacksReducer } from './attacks'
+import { charactersReducer } from './characters'
+import { spellsReducer } from './spells'
+import { equipmentReducer } from './equipment'
+import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+    attacks: attacksReducer,
+    characters: charactersReducer,
+    spells: spellsReducer,
+    equipment: equipmentReducer
+})
+
+export default rootReducer
