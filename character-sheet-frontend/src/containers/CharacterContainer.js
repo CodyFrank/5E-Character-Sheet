d5920cba309df9a1ff87a8106e9a058b40b02292
cody_frank <cody.frank30@gmail.com>
2020-02-25 19:07:26 -0500

    creates a character container component

---
+++ character-sheet-frontend/src/containers/CharacterContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class CharacterContainer extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>render a single character info here</h1>
+            </div>
+        )
+    }
+}
