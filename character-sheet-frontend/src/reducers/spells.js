74a85308ab9e1353da9e5bb2fa077f4e1300f024
cody_frank <cody.frank30@gmail.com>
2020-02-29 15:44:35 -0500

    updates spells reducer to handle async actions

--- character-sheet-frontend/src/reducers/spells.js
+++ character-sheet-frontend/src/reducers/spells.js
@@ -1,12 +1,26 @@
-import { ADD_SPELL, DELETE_SPELL } from '../actionTypes'
+import { ADD_SPELL, DELETE_SPELL, REQUESTING_SPELL_CHANGE } from '../actionTypes'
 
 
-export function spellsReducer(state=[], action){
+export function spellsReducer(state={ spells: [], requesting: false }, action){
     switch(action.type){
+        case REQUESTING_SPELL_CHANGE:
+            return {
+                ...state,
+                spells: [...state.spells],
+                requesting: true
+            }
         case ADD_SPELL:
-            return [ ...state, action.payload ]
+            return {
+                ...state,
+                spells: [ ...state.spells, action.payload ],
+                requesting: false
+            }
         case DELETE_SPELL:
-            return state.filter(s => s.id !== action.payload.id)
+            return {
+                ...state,
+                spells: state.spells.filter(s => s.id !== action.payload.id),
+                requesting: false
+            }
         default:
             return state
     }
