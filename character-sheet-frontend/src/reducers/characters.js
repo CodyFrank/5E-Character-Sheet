a9f3e0be80ba1e0a71dbe396bd63b782433d813f
cody_frank <cody.frank30@gmail.com>
2020-02-29 17:49:07 -0500

    updates character reducer

--- character-sheet-frontend/src/reducers/characters.js
+++ character-sheet-frontend/src/reducers/characters.js
@@ -2,20 +2,10 @@ import { ADD_CHARACTER, DELETE_CHARACTER, REQUESTING_CHARACTER_CHANGE } from '..
 
 export function charactersReducer(state=[], action){
     switch(action.type){
-        // case REQUESTING_CHARACTER_CHANGE:
-        //     return {
-        //         ...state, 
-        //         characters: [...state.characters],
-        //         requesting: true
-        //     }
         case ADD_CHARACTER:
             return [ ...state, action.payload ]
         case DELETE_CHARACTER:
-            return {
-                ...state,
-                characters: state.characters.filter(c => c.id !== action.payload.id),
-                requesting: false
-            }
+            return state.filter(c => c.id !== action.payload.id)
         default:
             return state
     }
