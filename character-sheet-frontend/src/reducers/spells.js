6fd5cc2514ab7203d8e58cfb3ef07b073fb9c33c
cody_frank <cody.frank30@gmail.com>
2020-02-29 17:54:07 -0500

    updates spells reducer

--- character-sheet-frontend/src/reducers/spells.js
+++ character-sheet-frontend/src/reducers/spells.js
@@ -1,26 +1,12 @@
-import { ADD_SPELL, DELETE_SPELL, REQUESTING_SPELL_CHANGE } from '../actionTypes'
+import { ADD_SPELL, DELETE_SPELL } from '../actionTypes'
 
 
-export function spellsReducer(state={ spells: [], requesting: false }, action){
+export function spellsReducer(state=[], action){
     switch(action.type){
-        case REQUESTING_SPELL_CHANGE:
-            return {
-                ...state,
-                spells: [...state.spells],
-                requesting: true
-            }
         case ADD_SPELL:
-            return {
-                ...state,
-                spells: [ ...state.spells, action.payload ],
-                requesting: false
-            }
+            return [ ...state, action.payload ]
         case DELETE_SPELL:
-            return {
-                ...state,
-                spells: state.spells.filter(s => s.id !== action.payload.id),
-                requesting: false
-            }
+            return state.filter(s => s.id !== action.payload.id)
         default:
             return state
     }
