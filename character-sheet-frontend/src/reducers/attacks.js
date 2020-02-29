13b6b70891195ca211de767e317ebd2904c8c7da
cody_frank <cody.frank30@gmail.com>
2020-02-29 11:46:47 -0500

    updates attack reducer to handle async actions

--- character-sheet-frontend/src/reducers/attacks.js
+++ character-sheet-frontend/src/reducers/attacks.js
@@ -1,12 +1,26 @@
-import { ADD_ATTACK, DELETE_ATTACK } from '../actionTypes'
+import { ADD_ATTACK, DELETE_ATTACK, REQUESTING_ATTACK_CHANGE } from '../actionTypes'
 
 
-export function attacksReducer(state=[], action){
+export function attacksReducer(state={ attacks: [], requesting: false }, action){
     switch(action.type){
+        case REQUESTING_ATTACK_CHANGE:
+            return {
+                ...state, 
+                attacks: [...state.attacks],
+                requesting: true
+            }
         case ADD_ATTACK:
-            return [ ...state, action.payload ]
+            return {
+                ...state, 
+                attacks: [ ...state, action.payload ],
+                requesting: false
+            }
         case DELETE_ATTACK:
-            return state.filter(a => a.id !== action.payload.id)
+            return {
+                ...state, 
+                attacks: state.attacks.filter(a => a.id !== action.payload.id),
+                requesting: false
+            }
         default:
             return state
     }
