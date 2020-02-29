f219a49a38c840667abd5872944d1063b35962cf
cody_frank <cody.frank30@gmail.com>
2020-02-29 12:11:10 -0500

    updates characters reducer for async actions

--- character-sheet-frontend/src/reducers/characters.js
+++ character-sheet-frontend/src/reducers/characters.js
@@ -1,11 +1,25 @@
-import { ADD_CHARACTER, DELETE_CHARACTER } from '../actionTypes'
+import { ADD_CHARACTER, DELETE_CHARACTER, REQUESTING_CHARACTER_CHANGE } from '../actionTypes'
 
-export function charactersReducer(state=[], action){
+export function charactersReducer(state={ characters:[], requesting: false }, action){
     switch(action.type){
+        case REQUESTING_CHARACTER_CHANGE:
+            return {
+                ...state, 
+                characters: [...state.characters],
+                requesting: true
+            }
         case ADD_CHARACTER:
-            return [ ...state, action.payload ]
+            return {
+                ...state,
+                characters: [ ...state.characters, action.payload ],
+                requesting: true
+            }
         case DELETE_CHARACTER:
-            return state.filter(c => c.id !== action.payload.id)
+            return {
+                ...state,
+                characters: state.characters.filter(c => c.id !== action.payload.id),
+                requesting: false
+            }
         default:
             return state
     }
