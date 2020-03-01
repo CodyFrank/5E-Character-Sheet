ed4134bb9ab519da9b109b687938a388af9f0390
cody_frank <cody.frank30@gmail.com>
2020-03-01 13:41:40 -0500

    creates controllers

--- character-sheet-api-backend/app/controllers/api/v1/equipment_controller.rb
+++ character-sheet-api-backend/app/controllers/api/v1/equipment_controller.rb
@@ -1,2 +1,33 @@
 class Api::V1::EquipmentController < ApplicationController
+    def create
+        character = Character.find_by(id: attack_params[:character_id])
+        if attack = character.attacks.build(attack_params)
+            render json: AttackSerializer.new(attack).to_serialized_json
+        else
+            render json: {message: "Creating that Attack failed"}
+        end
+    end
+
+    def update
+        attack = Attack.find_by(id: params[:id])
+        if Attack.update(attack_params)
+            render json: AttackSerializer.new(attack).to_serialized_json
+        else
+            render json: {message: "Cannot update that Attack"}
+        end
+    end
+
+    def destroy
+        if attack = Attack.find_by(id: params[:id]).destroy
+            render json: {attackId: attack.id}
+        else
+            render json: {message: "Cannot delete that attack"}
+        end
+    end
+
+    private
+
+    def attack_params
+        params.require(:attack).permit(:name, :attack_bonus, :damage, :damage_type, :character_id)
+    end
 end
