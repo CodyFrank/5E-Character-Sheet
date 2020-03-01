1dbe1734dcb378353a7ad590aed7cbe451babdbb
cody_frank <cody.frank30@gmail.com>
2020-03-01 13:33:37 -0500

    updates routes with namespace

--- character-sheet-api-backend/config/routes.rb
+++ character-sheet-api-backend/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :spells
-  resources :attacks
-  resources :equipment
-  resources :characters
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :characters
+      resources :spells, only: [:create, :update, :destroy]
+      resources :attacks, only: [:create, :update, :destroy]
+      resources :equipment, only: [:create, :update, :destroy]
+    end
+  end
 end
