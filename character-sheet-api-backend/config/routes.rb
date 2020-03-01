bcf02d27eb1a40e276783663fd55f82ee658e571
cody_frank <cody.frank30@gmail.com>
2020-03-01 13:25:10 -0500

    sets up rack-cors and creates db/migrates

--- character-sheet-api-backend/config/routes.rb
+++ character-sheet-api-backend/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :spells
+  resources :attacks
+  resources :equipment
+  resources :characters
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
