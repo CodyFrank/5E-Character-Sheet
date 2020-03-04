3ec5e5ac112eb77e0e5bb6db3d4cb1677b1bd290
cody <cody.frank30@gmail.com>
2020-03-03 21:20:22 -0500

    creates attack components

--- character-sheet-frontend/src/components/Attack.js
+++ character-sheet-frontend/src/components/Attack.js
@@ -4,7 +4,10 @@ export default class Attack extends React.Component{
     render(){
         return(
             <div>
-                <h1> info on an attack here </h1>
+                <p>name: {this.props.name}</p>
+                <p>attack bonus: {this.props.attack_bonus}</p>
+                <p>damage: {this.props.damage}</p>
+                <p>damage type: {this.props.damage_type}</p>
             </div>
         )
     }
--- character-sheet-frontend/src/containers/CharacterContainer.js
+++ character-sheet-frontend/src/containers/CharacterContainer.js
@@ -4,6 +4,7 @@ import { fetchCharacter } from '../actions'
 import { withRouter } from 'react-router-dom'
 import CharacterStats from '../components/CharacterStats.js'
 import Spell from '../components/Spell.js'
+import Attack from '../components/Attack.js'
 
 class CharacterContainer extends React.Component{
     constructor(props) {
@@ -32,12 +33,22 @@ class CharacterContainer extends React.Component{
         }
     }
 
+    renderCharacterAttacks = () => {
+        if(this.props.characters.length > 0){
+            const character = this.props.characters[0]
+            const attacks = character.attacks
+            console.log(attacks)
+            return attacks.map( a => <Attack key={`${character.id}${a.id}`} name={a.name} attack_bonus={a.attack_bonus} damage={a.damage} damage_type={a.damage_type}/>)
+        }
+    }
+
 
     render(){
         return(
             <div className={'CharacterContainer'}>
                 {this.renderCharacterStats()}
                 {this.renderCharacterSpells()}
+                {this.renderCharacterAttacks()}
                 <p>maps and renders all spells/attacks/equipment for a character</p>
                 <p>directs to a nested route for new attack/spell/equipment</p>
             </div>
