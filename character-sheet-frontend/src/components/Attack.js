22fc2d198d47ea6fd1a1f0326b01f7151c8ee82c
cody <cody.frank30@gmail.com>
2020-03-14 12:02:08 -0400

    creates make editable method in attack component and updates renderData

--- character-sheet-frontend/src/components/Attack.js
+++ character-sheet-frontend/src/components/Attack.js
@@ -17,11 +17,13 @@ export default class Attack extends React.Component{
           return <div>
             <label>Name: </label>
             <input onChange={this.handleNameChange} value={this.state.name}/>
-            <label>Level: </label>
-            <input onChange={this.handleLevelChange} value={this.state.level}/>
-            <label>Description: </label>
-            <textarea onChange={this.handleDescriptionChange} value={this.state.description}/>
-            <button onClick={this.handleSave}>Save Spell</button>
+            <label>Attack Bonus: </label>
+            <input onChange={this.handleAttackBonusChange} value={this.state.attack_bonus}/>
+            <label>Damage: </label>
+            <input onChange={this.handleDamageChange} value={this.state.damage}/>
+            <label>Damage Type: </label>
+            <input onChange={this.handleDamageTypeChange} value={this.state.damage_type}/>
+            <button onClick={this.handleSave}>Save Attack</button>
           </div>
         }else{
             return <div onClick={e => this.makeEditable()}>
@@ -31,6 +33,12 @@ export default class Attack extends React.Component{
                 <p>damage type: {this.props.damage_type}</p>
             </div>
         }
+      }
+
+      makeEditable = (e) => {
+        this.setState({
+          clicked: true
+        })
       }
 
 
