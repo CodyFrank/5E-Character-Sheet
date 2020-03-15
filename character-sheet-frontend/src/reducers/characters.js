2a78fa3994f3a3a8ce276f4a4885e1fc8a06721f
cody <cody.frank30@gmail.com>
2020-03-15 14:26:15 -0400

    updates readme and contributing and updates reducer to es6 variables

--- character-sheet-frontend/src/reducers/characters.js
+++ character-sheet-frontend/src/reducers/characters.js
@@ -11,6 +11,9 @@ import { ADD_CHARACTER,
          } from '../actionTypes'
 
 export function charactersReducer(state=[], action){
+    let newArray = []
+    let index
+    let character
     switch(action.type){
         case ADD_CHARACTERS:
             if(state.some(e => e.id === action.payload.id)){
@@ -23,44 +26,38 @@ export function charactersReducer(state=[], action){
         case DELETE_CHARACTER:
             return state.filter(c => c.id !== action.payload.characterId )
         case ADD_SPELL:
-            var newArray = []
-            var index = state.findIndex(e => e.id === action.payload.character_id)
-            var character = state[index]
+            index = state.findIndex(e => e.id === action.payload.character_id)
+            character = state[index]
             character.spells.push(action.payload)
             newArray.push(character)
             return newArray
         case ADD_ATTACK:
-            var newArray = []
-            var index = state.findIndex(e => e.id === action.payload.character_id)
-            var character = state[index]
+            index = state.findIndex(e => e.id === action.payload.character_id)
+            character = state[index]
             character.attacks.push(action.payload)
             newArray.push(character)
             return newArray
         case ADD_EQUIPMENT:
-            var newArray = []
-            var index = state.findIndex(e => e.id === action.payload.character_id)
-            var character = state[index]
+            index = state.findIndex(e => e.id === action.payload.character_id)
+            character = state[index]
             character.equipment.push(action.payload)
             newArray.push(character)
             return newArray
         case DELETE_SPELL:
-            var newArray = []
-            var index = state.findIndex(e => e.id === action.payload.characterId)
-            var character = state[index]
+            index = state.findIndex(e => e.id === action.payload.characterId)
+            character = state[index]
             character.spells = character.spells.filter(s => s.id !== action.payload.spellId)
             newArray.push(character)
             return newArray
         case DELETE_ATTACK:
-            var newArray = []
-            var index = state.findIndex(e => e.id === action.payload.characterId)
-            var character = state[index]
+            index = state.findIndex(e => e.id === action.payload.characterId)
+            character = state[index]
             character.attacks = character.attacks.filter(s => s.id !== action.payload.attackId)
             newArray.push(character)
             return newArray
         case DELETE_EQUIPMENT:
-            var newArray = []
-            var index = state.findIndex(e => e.id === action.payload.characterId)
-            var character = state[index]
+            index = state.findIndex(e => e.id === action.payload.characterId)
+            character = state[index]
             character.equipment = character.equipment.filter(s => s.id !== action.payload.equipmentId)
             newArray.push(character)
             return newArray
